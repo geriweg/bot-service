c6c4fa29f8f609b517edcde1b75fced32330ac6a
geri <gwegleitner@gmail.com>
2019-09-06 19:00:06 +0200

    Initial commit

---
+++ src/main/java/geri/roboleague/bot/botservice/BotServiceApplication.java
@@ -0,0 +1,13 @@
+package geri.roboleague.bot.botservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotServiceApplication.class, args);
+	}
+
+}
